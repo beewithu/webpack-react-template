7c08addfbc610aa3dffcdda770052dc73b56cea0
Phong Tran <phong.tran1@tiki.vn>
2022-03-31 15:56:53 +0700

    remove preload state

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,31 +2,14 @@ import { init, RematchDispatch, RematchRootState } from '@rematch/core';
 import models from './models';
 import RootModel from './models/rootModel';
 
-declare global {
-  interface Window {
-    __PRELOADED_STATE__: any;
-    snapSaveState: () => void;
-  }
-}
-// Grab the state from a global variable injected into the server-generated HTML
-const preloadedState = window.__PRELOADED_STATE__;
-
-// Allow the passed state to be garbage-collected
-delete window.__PRELOADED_STATE__;
-
 const store = init({
   redux: {
-    initialState: preloadedState,
     devtoolOptions: {
       actionSanitizer: (action) => action,
       disabled: process.env.NODE_ENV === 'production',
     },
   },
   models,
-});
-
-window.snapSaveState = () => ({
-  __PRELOADED_STATE__: store.getState(),
 });
 
 export type Store = typeof store;
