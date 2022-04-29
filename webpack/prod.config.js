e3b9e8129ce525f283c06d46f151d852fa68cafa
Phong Tran <phong.tran1@tiki.vn>
2022-04-29 11:09:39 +0700

    add esbuild minify plugin

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,10 +1,16 @@
 const common = require('./common.config');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: 'es2015',
+      }),
+    ],
     splitChunks: {
       chunks: 'all',
       cacheGroups: {
